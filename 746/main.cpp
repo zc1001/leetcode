a72acb05725dc1866d63fdfed69e5613ba494deb
zc1001 <jlbczhangchao0800@126.com>
2019-08-23 09:52:35 +0800

    使用最小花费爬楼梯

--- 746/main.cpp
+++ 746/main.cpp
@@ -1,3 +1,18 @@
+/*
+使用最小力气爬楼梯
+DP
+使用两个队列记录这一个阶梯走或不走
+opt0 是第i个楼梯走的情况
+opt1 是第i个楼梯不走的情况
+初始化opt[0]、opt[1]
+从opt[2]开始进行
+走的情况 判断opt0[i-2] + cost[i]    opt0[i-1] + cost[i] (之前走的最佳情况)  opt1[i-1] +cost[i-1] +cost[i]   opt1[i-2] +cost[i-2] +cost[i]   （之前不走的最佳情况，这之中要包括i-1步骤不走的情况下需要加上cost[i-1]）
+opt1 的最佳情况为第i步不走，所以opt1[i] = opt0[i-1]
+最后比较opt0和opt1的最后一个元素
+*/
+
+
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -29,7 +44,7 @@ public:
             num = (opt1[i-2]+cost[i-2]+cost[i]);
          opt0.push_back(num);
          opt1.push_back(opt0[i-1]);
-         cout<<num<<endl;
+         //cout<<num<<endl;
         }
         if(opt0[opt0.size()-1]<opt1[opt1.size()-1])
              return opt0[opt0.size()-1];
