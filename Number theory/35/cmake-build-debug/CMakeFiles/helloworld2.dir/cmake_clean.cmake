882f24cfdbab44119cd490c2ca4f3cf2e1d34adb
zc1001 <jlbczhangchao0800@126.com>
2019-10-07 20:28:14 +0800

    搜索插入位置 又做了一道简单题。。。。。二分

---
+++ Number theory/35/cmake-build-debug/CMakeFiles/helloworld2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld2.dir/main.cpp.o"
+  "helloworld2"
+  "helloworld2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
