fcb7a98429dcdc9fac1404693dd67bd0c12b4edd
zc1001 <jlbczhangchao0800@126.com>
2019-10-11 20:56:26 +0800

    平方根 直接返回......九点下班凑时间来一简单题

---
+++ Number theory/69/69.py
@@ -0,0 +1,45 @@
+import  math
+
+# 直接return
+#执行用时 :32 ms, 在所有 Python 提交中击败了48.75% 的用户
+#内存消耗 :11.8 MB, 在所有 Python 提交中击败了18.86%的用户
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+    p = Solution()
+
+    #p.compare(A,B)
+
+
+
+
+
+    #print(s)
+    #print(test.lastSubstring("abab"))
