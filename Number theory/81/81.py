47059ff571084d20fad60ae14774254a39f0e740
zc1001 <jlbczhangchao0800@126.com>
2019-10-15 09:01:54 +0800

    搜索旋转排序数组||

---
+++ Number theory/81/81.py
@@ -0,0 +1,35 @@
+import  math
+
+# 二分 python 可暴力
+# 执行用时 :40 ms, 在所有 python 提交中击败了97.74% 的用户
+# 内存消耗 :12.1 MB, 在所有 python 提交中击败了7.27%的用户
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        return target in nums
+if __name__ == '__main__':
+
+
+
+    n = [5, 7, 7, 8, 8, 10]
+    matrix = [
+        [1, 3, 5, 7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 50]
+    ]
+    p = Solution()
+
+ 
+
+    #p.compare(A,B)
+
+
+
+
+
+    #print(s)
+    #print(test.lastSubstring("abab"))
